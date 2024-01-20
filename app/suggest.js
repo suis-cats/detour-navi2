270443c31477c7c8b3da87814bb255a69c8c686f
suis-cats <takkunkimidori@gmail.com>
2024-01-20 10:21:16 +0900

    create page

---
+++ app/suggest.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SuggestPage = () => {
+  return (
+    <div>
+      <h1>Suggest Page</h1>
+      {/* Add your content here */}
+      <button className="yes-button">Button 1</button>
+      <button className="no-button">Button 2</button>
+    </div>
+  );
+};
+
+export default SuggestPage;
